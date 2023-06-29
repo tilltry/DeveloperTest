dbab2f9bffadeccdd9a841f5251101dd1f28287b
tilltry <138111833+tilltry@users.noreply.github.com>
2023-06-29 15:43:11 +0300

    Update FirstTask.cs

--- FirstTask.cs
+++ FirstTask.cs
@@ -2,32 +2,64 @@ namespace Library
 {
     public class ZadanieOne
     {
-        public void One()
+        private static int n;
+        private static string result;
+        public static void One()
         {
-            Console.Write("Введите число N: ");
-            int n = int.Parse(Console.ReadLine());
-            string result = string.Join(", ", Enumerable.Range(1, n));
-            Console.WriteLine(result);
+            try
+            {
+                Console.Write("Введите число N: ");
+                n = int.Parse(Console.ReadLine());
+                if (n >= 1)
+                {
+                    result = string.Join(",", Enumerable.Range(1, n)) + '.';
+                    Console.WriteLine(result);
+                }
+                else
+                {
+                    Console.Write("Неверный ввод");
+                }
+            }
+            catch
+            {
+                Console.Write("Неверный ввод");
+            }
+            
         }
     }
     public class ZadanieTwo
     {
+        private static int n;
         public static void Two()
         {
-            Console.Write("Введите нечетное число N: ");
-            int n = int.Parse(Console.ReadLine());
-            if (n % 2 == 0) // If N is an even number, exit the function.
+            try
             {
-                Console.WriteLine("Вы ввели четное N.");
-                return;
-            }            
-            for (int row = 1; row <= n; row++) {
-                for (int col = 1; col <= n; col++) {
-                    if (row == n / 2 + 1 && col == n / 2 + 1) Console.Write(" ");
-                    else Console.Write("#");                    
+                Console.Write("Введите нечетное число N: ");
+                n = int.Parse(Console.ReadLine());
+                if (n % 2 == 0 || n < 3) // If N is an even number, exit the function.
+                {
+                    Console.Write("Неверный ввод");
+                    return;
                 }
-                Console.WriteLine();
+                for (int row = 0; row < n; row++)
+                {
+                    for (int col = 0; col < n; col++)
+                    {
+                        if (row == n / 2 && col == n / 2)
+                        {
+                            Console.Write(" ");
+                        }
+                        else Console.Write("#");
+                    }
+                    Console.WriteLine();
+                }
             }
+            catch
+            {
+                Console.Write("Неверный ввод");
+            }
+           
+            
         }
     }
 }
